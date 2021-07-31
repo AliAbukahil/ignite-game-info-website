123943d05e9947823df03e76dcba26aa6985a156
ali.abukahil <ali.abukahil@gmail.com>
2021-07-31 22:42:29 +0200

    4. Action Creators,Redux and Thunk

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Redux Setup
-import { createStore } from "redux";
+// Redux Setup + Redux-thunk, which are (applyMiddleware, compose)
+import { createStore, applyMiddleware, compose } from "redux";
 
 //importing that combiner of reducers
 import rootReducer from "./reducers";
@@ -11,11 +11,13 @@ import rootReducer from "./reducers";
 // importing the connector ot the connection  to Redux which is Provider (this how you connect your app to Redux)
 import { Provider } from "react-redux";
 
+// Importing thunk from redux-thunk
+import thunk from "redux-thunk";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 //Redux devtools, you add it in the createStore parameters with the Reducers combiner ==> (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
